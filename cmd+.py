ab15647d1910286c0da869319e47d834898e2556
JezzaProto <54776821+JezzaProto@users.noreply.github.com>
2020-11-27 14:30:44 +0000

    Added main files

---
+++ cmd+.py
@@ -0,0 +1,328 @@
+import os, sys, ctypes, shutil, subprocess, json, pathlib
+from os import system, name
+from time import sleep
+from datetime import datetime
+from winreg import ConnectRegistry, OpenKey, QueryValueEx, HKEY_CURRENT_USER
+
+def clear():
+    if checkIdle():
+        for _ in range(30):
+            print("\n")
+    elif name == "nt":
+        _ = system("cls")
+    else:
+        _ = system("clear")
+
+def rootPath():
+    path = sys.executable
+    while os.path.split(path)[1]:
+        path = os.path.split(path)[0]
+    return path
+
+def checkIdle():
+    if "idlelib.run" in sys.modules:
+        return True
+    return False
+
+def setColour(colour):
+    if checkIdle():
+        return False
+    handle = ctypes.windll.kernel32.GetStdHandle(-11)
+    return ctypes.windll.kernel32.SetConsoleTextAttribute(handle, colour)
+
+def error(msg):
+    if checkIdle():
+        print(msg)
+    else:
+        setColour(RED)
+        print(msg)
+        setColour(WHITE)
+
+def info(msg):
+    if checkIdle():
+        print(msg)
+    else:
+        setColour(GOLD)
+        print(msg)
+        setColour(WHITE)
+
+try:
+    Key = r"Software\Policies\Microsoft\Windows\System"
+    Reg = ConnectRegistry(None, HKEY_CURRENT_USER)
+    CMDDisableKey = OpenKey(Reg, Key)
+    CMDDisableVal = QueryValueEx(CMDDisableKey, "DisableCMD")[0]
+    if CMDDisableVal == 1:
+        print("CMD and scripts are disabled. This program will not work.")
+        sys.exit(1)
+except FileNotFoundError:
+    pass
+
+print("Setting up...")
+
+# Colours
+DARK_BLUE = 0x01
+DARK_GREEN = 0x02
+BLUE = 0x03
+RED = 0x04
+DARK_PURPLE = 0x05
+GOLD = 0x06
+WHITE = 0x07
+GREY = 0x08
+DARK_BLUE = 0x09
+GREEN = 0x0a
+LIGHT_BLUE = 0x0b
+LIGHT_RED = 0x0c
+PURPLE = 0x0d
+YELLOW = 0x0e
+
+Yes = ["Y", "YES"]
+All = ["*.*", "all"]
+
+try:
+    with open("settings.json", "r") as file:
+        Settings = json.load(file)
+    SettingsFile = True
+    print("Settings file found.")
+except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
+    error(f"An error has occured.\nError: {e}\nRunning without aliases")
+    SettingsFile = False
+
+if SettingsFile:
+    try:
+        if Settings["tqdm"]:
+            try:
+                from tqdm import tqdm
+                TQDMInstall = True
+            except ImportError:
+                TQDMInstall = False
+        else:
+            TQDMInstall = False
+    except KeyError:
+        TQDMInstall = False
+
+global FileLoc
+FileLoc = os.getcwd()
+
+os.chdir(rootPath())
+
+print("Command prompt setup.\nEnter \"exit\" to quit.")
+
+if not checkIdle():
+    ctypes.windll.kernel32.SetConsoleTitleW("CMD+")
+
+while True:
+    UserInput = input(f"CMD+: {os.getcwd()}> ")
+    Command = UserInput.split(" ")[0]
+    AdminMode = False
+    if len(Command) == 0:
+            print()
+            continue
+    try:
+        Args = UserInput.split(" ")[1:]
+    except:
+        pass
+    if SettingsFile:
+        try:
+            Command = Settings["aliases"][Command]
+        except KeyError:
+            pass
+    if Command == "explorer":
+        if len(Args) == 0:
+            print(f"Opening explorer in {os.getcwd()}")
+            system(f'explorer %cd%')
+            continue
+        Dir = "\\".join(Args)
+        print(f"Opening explorer in {Dir}")
+        system(f'start "{os.path.realpath(Dir)}"')
+        continue
+    elif Command == "cd":
+        if len(Args) == 0:
+            print(f"Current Directory: {os.getcwd()}")
+            continue
+        cdArgs = " ".join(Args)
+        try:
+            os.chdir(cdArgs)
+            print(f"Changed directory to {os.getcwd()}")
+            continue
+        except (FileNotFoundError, PermissionError) as e:
+            print(f"Error changing directory.\nError: {e}")
+            continue
+    elif Command in ["cls", "clear"]:
+        clear()
+        continue
+    try:
+        if Command[1] == ":" and Command[0].isalpha():
+            os.chdir(Command)
+            os.chdir("\\")
+            print(f"Changed directory to {os.getcwd()}")
+            continue
+    except:
+        pass
+    if Command == "title":
+        if checkIdle():
+            print("Title command not supported inside IDLE.")
+            continue
+        ctypes.windll.kernel32.SetConsoleTitleW(" ".join(Args))
+        continue
+    elif Command == "del":
+        if len(Args) == 0:
+            error("Deletion command missing arguments.")
+            continue
+        if Args[0] in All:
+            info("Are you sure you want to delete this directories contents? Y/N")
+            confirm = input()
+            if confirm.upper() in Yes:
+                if TQDMInstall:
+                    for dirpath, dirnames, filenames in tqdm(os.walk(os.getcwd(), topdown=False), ascii=True, desc="Deletion Progress"):
+                        try:
+                            os.rmdir(dirpath)
+                        except OSError as ex:
+                            error(f"Error deleting {dirpath}:\n{ex}")
+                    continue
+                else:
+                    for dirpath, dirnames, filenames in os.walk(os.getcwd(), topdown=False):#
+                        for file in filenames:
+                            try:
+                                os.remove(file)
+                            except OSError as ex:
+                                error(f"Error deleting {file}:\n{ex}")
+                        try:
+                            os.rmdir(dirpath)
+                        except OSError as ex:
+                            error(f"Error deleting {file}:\n{ex}")
+                    continue
+            continue
+        else:
+            if not os.access(" ".join(Args), os.F_OK):
+                error("This file does not exist.")
+                continue
+            if not os.access(" ".join(Args), os.W_OK):
+                error("You do not have permissions to delete this file.")
+                continue
+            info(f"Are you sure you want to delete {' '.join(Args)}? Y/N")
+            confirm = input()
+            if confirm.upper() in Yes:
+                try:
+                    if os.path.isdir(" ".join(Args)):
+                        os.rmdir(" ".join(Args))
+                    else:
+                        os.remove(" ".join(Args))
+                except (IsADirectoryError, OSError, WindowsError) as e:
+                    error(f"Could not remove file.\nError: {e}")
+                    continue
+            continue
+    elif Command == "dir":
+        DirArgs = " ".join(Args)
+        output = os.popen(f"dir /a {DirArgs}").read()
+        print(output)
+        continue
+    elif Command == "aliases":
+        if len(Args) == 0:
+            print("{:<15} | {:<15}".format("Command","Alias"))
+            print("=-=-=-=-=-=-=-=-|-=-=-=-=-=-=")
+            for Key, Item in Settings["aliases"].items():
+                print("{:<15} | {:<15}".format(Key, Item))
+            continue
+        if Args[0] == "add":
+            try:
+                Command = Args[1]
+                Alias = Args[2]
+            except IndexError:
+                print("Missing arguments.")
+                continue
+            try:
+                OldCommand = Settings["aliases"][Command]
+                if OldCommand == Alias:
+                    print("This command is already aliased to this alias.")
+                    continue
+                Confirm = input(f"This command is already aliased to {OldCommand}.\nDo you want to overwrite it?\n")
+                if Confirm.upper() in Yes:
+                    Settings["aliases"][Command] = Alias
+                    print(f"\"{Command}\" alias updated to \"{Alias}\".")
+                    continue
+                else:
+                    print("Not continuing.")
+                    continue
+            except (IndexError, KeyError):
+                Settings["aliases"][Command] = Alias
+                print(f"\"{Command}\" alias set to \"{Alias}\".")
+                continue
+        elif Args[0] == "remove":
+            try:
+                Command = Args[1]
+            except IndexError:
+                print("Missing arguments.")
+                continue
+            try:
+                OldCommand = Settings["aliases"][Command]
+                Confirm = input(f"This command is aliased to {OldCommand}.\nDo you want to remove it?\n")
+                if Confirm.upper() in Yes:
+                    del Settings["aliases"][Command]
+                    print(f"{Command} alias removed.")
+                    continue
+                else:
+                    print("Not continuing.")
+                    continue
+            except KeyError:
+                print("This is not an alias.")
+                continue
+        elif Args[0] == "view":
+            try:
+                Command = Args[1]
+            except IndexError:
+                print("{:<15} | {:<15}".format("Command","Alias"))
+                print("=-=-=-=-=-=-=-=-|-=-=-=-=-=-=")
+                for Key, Item in Settings["aliases"].items():
+                    print("{:<15} | {:<15}".format(Key, Item))
+                continue
+            try:
+                OldCommand = Settings["aliases"][Command]
+                print(f"\"{Command}\" is aliased to \"{OldCommand}\"")
+                continue
+            except KeyError:
+                print(f"\"{Command}\" is not aliased.")
+                continue
+    elif Command == "reload":
+        OldDir = os.getcwd()
+        os.chdir(FileLoc)
+        try:
+            with open("settings.json", "r") as file:
+                Settings = json.load(file)
+            SettingsFile = True
+            print("Settings file reloaded.")
+        except (json.decoder.JSONDecodeError, FileNotFoundError) as e:
+            error(f"An error has occured.\nError: {e}\nRunning without aliases")
+            SettingsFile = False
+            continue
+        if SettingsFile:
+            try:
+                if Settings["tqdm"]:
+                    try:
+                        from tqdm import tqdm
+                        TQDMInstall = True
+                    except ImportError:
+                        TQDMInstall = False
+                else:
+                    TQDMInstall = False
+            except KeyError:
+                TQDMInstall = False
+        os.chdir(OldDir)
+        continue
+    elif Command == "exit":
+        break
+    try:
+        output = os.popen(UserInput).read()
+    except KeyboardInterrupt:
+        print("Command cancelled.")
+        continue
+    print(output)
+
+print("Saving changed aliases, settings etc.")
+setColour(WHITE)
+os.chdir(FileLoc)
+tmp = {}
+tmp["tqdm"] = Settings["tqdm"]
+tmp["aliases"] = Settings["aliases"]
+with open("settings.json", "w") as File:
+    json.dump(tmp, File, indent=4, sort_keys=True)
+print("Settings saved. Quitting.")
